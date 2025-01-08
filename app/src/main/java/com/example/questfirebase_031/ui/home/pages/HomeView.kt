2c10547405d1c5fdf0a3072dfbb53455e2f9325b
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-08 18:49:06 +0700

    memperbaiki bug pada AndroidManifest.xml dan HomeView.kt

--- app/src/main/java/com/example/questfirebase_031/ui/home/pages/HomeView.kt
+++ app/src/main/java/com/example/questfirebase_031/ui/home/pages/HomeView.kt
@@ -1,6 +1,5 @@
 package com.example.questfirebase_031.ui.home.pages
 
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
