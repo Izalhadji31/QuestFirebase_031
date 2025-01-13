fbb9d6e18edf60eb8fdf26bf552426715d1152ff
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-13 08:10:57 +0700

    menambahkan dependencies agar PengelolaHalaman tidak eror

--- app/src/main/java/com/example/questfirebase_031/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/questfirebase_031/ui/navigation/PengelolaHalaman.kt
@@ -2,6 +2,10 @@ package com.example.questfirebase_031.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.questfirebase_031.ui.home.pages.HomeScreen
 import com.example.questfirebase_031.ui.insert.pages.InsertMhsView
 
@@ -13,17 +17,16 @@ fun PengelolaHalaman(
     NavHost(
         navController = navController,
         startDestination = DestinasiHome.route,
-        modifier = Modifier
-    ) {
+        modifier = modifier
+    ){
         composable(DestinasiHome.route){
             HomeScreen(
                 navigateToItemEntry = {
                     navController.navigate(DestinasiInsert.route)
-                }
+                 },
             )
         }
-
-        omposable(DestinasiInsert.route){
+        composable(DestinasiInsert.route){
             InsertMhsView(
                 onBack = { navController.popBackStack() },
                 onNavigate = { navController.navigate(DestinasiHome.route) }
