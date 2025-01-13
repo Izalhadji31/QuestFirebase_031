b10ed77d9f9a1653db2836a761edc658c395b394
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-13 07:27:08 +0700

    fun updateState: memperbaharui state berdasarkan input pengguna

--- app/src/main/java/com/example/questfirebase_031/ui/insert/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/questfirebase_031/ui/insert/viewmodel/InsertViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.questfirebase_031.ui.insert.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.questfirebase_031.repository.RepositoryMhs
+
+class InsertViewModel(
+    private val repositoryMhs: RepositoryMhs
+): ViewModel() {
+
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    fun updateState(mahasiswaEvent: MahasiswaEvent){
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
+
+
+}
